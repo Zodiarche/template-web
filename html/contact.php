9136542f1603eedde6c22262bba758c205dbb761
Benjamin <b.guillemin@agencebuzz.com>
2024-06-27 12:25:21 +0200

    MAJ Template

--- html/contact.php
+++ html/contact.php
@@ -18,77 +18,81 @@ session_start();
       <section id="main-section">
         <div class="wrapper">
           <div id="form-container">
-            <h1 class="animation" data-animation="fadeIn">Contact</h1>
+            <h1 class="" data-animation="">Contact</h1>
 
             <form id="form" action="<?php echo $page ?>" method="post">
               <?php
-              if (isset($_GET['send']) && !empty($_GET['send'])) {
-                if ($_GET['send'] == "contact_success") {
-                  echo '<div class="states success" role="alert" style="text-align: left;"><p>Votre message a bien été envoyé.<br>Nous vous répondrons dans les brefs délais.</p></div>';
-                }
-              }
+              // if (isset($_GET['send']) && !empty($_GET['send'])) {
+              //   if ($_GET['send'] == "contact_success") {
+              //     echo '<div class="states success" role="alert" style="text-align: left;"><p>Votre message a bien été envoyé.<br>Nous vous répondrons dans les brefs délais.</p></div>';
+              //   }
+              // }
               ?>
 
-              <?php require_once "./components/formulaire/contact.php" ?>
+              <?php
+              echo '<div class="states info" role="alert" style="text-align: left;"><p>Ce formulaire n\'est pas relié à une BDD en ligne. Le remplir sert donc à rien, le code est néanmoins prêt à être utilisé.</p></div>';
+              ?>
+
+              <?php /* require_once "./components/formulaire/contact.php" */ ?>
 
               <div id="contact-form" class="grid-field-container">
-                <div class="field" class="animation" data-animation="moveToLeft">
+                <div class="field" class="" data-animation="">
                   <label for="lastname">
                     <input type="text" name="lastname" id="lastname" placeholder="Nom *">
                   </label>
                 </div>
 
-                <div class="field" class="animation" data-animation="moveToRight">
+                <div class="field" class="" data-animation="">
                   <label for="firstname">
                     <input type="text" name="firstname" id="firstname" placeholder="Prénom *">
                   </label>
                 </div>
 
-                <div class="field" class="animation" data-animation="moveToLeft">
+                <div class="field" class="" data-animation="">
                   <label for="email-contact">
                     <input type="text" name="email-contact" id="email-contact" placeholder="Adresse mail *">
                   </label>
                 </div>
 
-                <div class="field" class="animation" data-animation="moveToRight">
+                <div class="field" class="" data-animation="">
                   <label for="phone">
                     <input type="tel" name="phone" id="phone" placeholder="Téléphone *">
                   </label>
                 </div>
               </div>
 
-              <div class="field" class="animation" data-animation="moveToRight">
+              <div class="field" class="" data-animation="">
                 <label for="address">
                   <input type="text" name="address" id="address" placeholder="Adresse *">
                 </label>
               </div>
 
               <div id="contact-form" class="grid-field-container">
-                <div class="field" class="animation" data-animation="moveToLeft">
+                <div class="field" class="" data-animation="">
                   <label for="town">
                     <input type="text" name="town" id="town" placeholder="Ville *">
                   </label>
                 </div>
 
-                <div class="field" class="animation" data-animation="moveToRight">
+                <div class="field" class="" data-animation="">
                   <label for="postal-code">
                     <input type="text" name="postal-code" id="postal-code" placeholder="Code postal *">
                   </label>
                 </div>
               </div>
 
-              <div class="field" class="animation" data-animation="moveToLeft">
+              <div class="field" class="" data-animation="">
                 <label for="subject"></label>
                 <input type="text" name="subject" id="subject" placeholder="Sujet *">
               </div>
 
-              <div class="field" class="animation" data-animation="scaleUp">
+              <div class="field" class="" data-animation="">
                 <label for="message">
                   <textarea name="message" id="message" placeholder="Message" rows="10"></textarea>
                 </label>
               </div>
 
-              <div class="field" class="animation" data-animation="scaleDown">
+              <div class="field" class="" data-animation="">
                 <label for="rgpd" class="flex-rgpd-container">
                   <input type="checkbox" id="rgpd" name="rgpd" value="OUI, j'accepte que mes données personnelles soient stockées, traitées et utilisées dans le cadre de la communication qui peut en découler.">
                   Oui, j'accepte que mes données personnelles soient stockées, traitées et utilisées dans le cadre de la communication qui peut en découler.
--- html/index.php
+++ html/index.php
@@ -16,24 +16,24 @@ $description = "";
       <section id="main-section">
         <div class="wrapper">
           <div>
-            <h1 class="animation" data-animation="moveToDown">Titre de niveau 1</h1>
-            <h2 class="animation" data-animation="moveToRight">Titre de niveau 2</h2>
-            <h3 class="animation" data-animation="moveToLeft">Titre de niveau 3</h3>
-            <h4 class="animation" data-animation="moveToUp">Titre de niveau 4 avec une balise small <small>(ne doit pas être plus petit que le texte courant)</small></h4>
+            <h1 class="" data-animation="">Titre de niveau 1</h1>
+            <h2 class="" data-animation="">Titre de niveau 2</h2>
+            <h3 class="" data-animation="">Titre de niveau 3</h3>
+            <h4 class="" data-animation="">Titre de niveau 4 avec une balise small <small>(ne doit pas être plus petit que le texte courant)</small></h4>
 
-            <p class="animation" data-animation="flash">Paragraphe de taille 16px (1.6rem). Peut descendre jusqu'à un minimum de 14px.</p>
+            <p class="" data-animation="">Paragraphe de taille 16px (1.6rem). Peut descendre jusqu'à un minimum de 14px.</p>
 
-            <p class="animation" data-animation="scaleDown"><img class="float-right animation visible" data-animation="bounce" src="https://via.placeholder.com/400x300.png" alt="">Une image avec un ratio de 4/3 !</p>
+            <p class="" data-animation=""><img class="float-right" data-animation="" src="https://via.placeholder.com/400x300.png" alt="">Une image avec un ratio de 4/3 !</p>
 
             <ul>
-              <li class="animation" data-animation="scaleUp">Elément de liste 1</li>
-              <li class="animation" data-animation="scaleDown">Elément de liste 2 avec <a href="#">un lien</a> avec au moins un :hover et :focus, le :active est optionnel.</li>
-              <li class="animation" data-animation="scaleUp">Elément de <strong>liste 3</strong></li>
+              <li class="" data-animation="">Elément de liste 1</li>
+              <li class="" data-animation="">Elément de liste 2 avec <a href="#">un lien</a> avec au moins un :hover et :focus, le :active est optionnel.</li>
+              <li class="" data-animation="">Elément de <strong>liste 3</strong></li>
             </ul>
 
-            <p class="animation" data-animation="fadeIn"><a href="index.php" class="btn">Un lien qui doit apparaitre comme un bouton</a></p>
+            <p class="" data-animation=""><a href="index.php" class="btn">Un lien qui doit apparaitre comme un bouton</a></p>
 
-            <blockquote cite="https://fr.wikiquote.org/wiki/La_Folie_des_grandeurs" class="animation visible" data-animation="pulse">
+            <blockquote cite="https://fr.wikiquote.org/wiki/La_Folie_des_grandeurs" class="" data-animation="">
               <p>Mais qu'est-ce que je vais devenir ? Je suis ministre, je ne sais rien faire !</p>
               <p>Louis de Funès - <cite>La Folie des grandeurs</cite> (1971), écrit par Gérard Oury</p>
             </blockquote>
